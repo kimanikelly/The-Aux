d3b5ef506da3cc0a431ae83a1397c049bc830692
kimani kelly <kimanikelly@yahoo.com>
2020-01-13 06:55:27 -0500

    Spotify credentials stored in the .env file and imported to server.js

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+// Requiring the path module as a dependency
+var path = require('path');
+
+// Loads environment variables from the .env file into process.env
+// Configures dotenv
+require('dotenv').config();
+
 // Imports the express module
 var express = require('express');
 
@@ -5,11 +12,36 @@ var express = require('express');
 var app = express();
 
 // Sets the port the express server will be running on
-var PORT = process.env.PORT = 3001;
+var PORT = process.env.PORT = 3000;
+
+// Imports the Spotify credentials 
+var spotifyId = require('./spotifyCredentials');
+
+// Stores the Spotify client id
+var clientId = spotifyId.credentials.id;
+
+// Stores the Spotify redirect uri
+var redirectUri = spotifyId.credentials.redirectUri;
 
 // Epxress middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+app.get('/login', function (req, res) {
+    console.log(clientId);
+    console.log(redirectUri);
+    var scopes = 'user-read-private user-read-email';
+    res.redirect('https://accounts.spotify.com/authorize' +
+        '?response_type=code' +
+        '&client_id=' + clientId +
+        (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+        '&redirect_uri=' + encodeURIComponent(redirectUri));
+});
+
+app.get('/home', function (req, res) {
+    res.json('This is working')
+})
 
 // Starts the express server
 app.listen(PORT, function () {
---
+++ spotifyCredentials.js
@@ -0,0 +1,4 @@
+exports.credentials = {
+    id:process.env.SPOTIFY_CLIENT_ID,
+    redirectUri:process.env.SPOTIFY_REDIRECT_URI
+};
