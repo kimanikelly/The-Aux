9ef0bf1ea80086e816eb10fedc422f3defbec72c
kimani kelly <kimanikelly@yahoo.com>
2020-02-02 14:23:20 -0500

    The React Login component rendered

--- client/src/components/Login/login.js
+++
@@ -1,8 +0,0 @@
-// Allows for React JSX to be used
-// JSX is the React extension for HTML
-import react from 'react';
-
-// ES6 class to define the Login component
-class Login extends React.Component {
-
-};
---
+++ client/src/login.js
@@ -0,0 +1,21 @@
+// Allows for React JSX to be used
+// JSX is the React extension for HTML
+import React from 'react';
+
+// Loads the CSS stylesheet into login.js
+import './style.css';
+
+
+// ES6 class to define the Login component
+class Login extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Testing</h1>
+            </div>
+        )
+    }
+};
+
+// Exports the Login component
+export default Login;
