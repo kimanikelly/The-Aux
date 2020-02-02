34025b635061aaf3882a78bf10c838b8f168c277
kimani kelly <kimanikelly@yahoo.com>
2020-02-02 12:40:16 -0500

    Style.css created for the Login component

--- client/src/components/Login/login.js
+++ client/src/components/Login/login.js
@@ -1,3 +1,8 @@
-// Allows JSX to be used
+// Allows for React JSX to be used
+// JSX is the React extension for HTML
 import react from 'react';
 
+// ES6 class to define the Login component
+class Login extends React.Component {
+
+};
