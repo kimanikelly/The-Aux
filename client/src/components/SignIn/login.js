9f87b9c8d99d589af4245b54e8806b885675fd10
kimani kelly <kimanikelly@yahoo.com>
2020-02-02 14:32:03 -0500

    React side file paths for the components directory need to be fixed

--- client/src/components/SignIn/login.js
+++ client/src/components/SignIn/login.js
@@ -5,7 +5,6 @@ import React from 'react';
 // Loads the CSS stylesheet into login.js
 import './style.css';
 
-
 // ES6 class to define the Login component
 class Login extends React.Component {
     render() {
