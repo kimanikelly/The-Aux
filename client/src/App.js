26431dd677c30fc5d05839b9eaa5339151a7eb43
kimani kelly <kimanikelly@yahoo.com>
2020-02-02 14:43:07 -0500

    Changed SignIn component name back to Login to test if the it will render

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Login from './components/Login';
+import Login from './components/Login/login';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Login/>
+        <Login />
       </div>
     )
   }
