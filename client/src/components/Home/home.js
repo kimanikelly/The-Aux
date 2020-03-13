b53b0275cf6d2098d807ea7be74ffdf20e14573a
kimani kelly <kimanikelly@yahoo.com>
2020-03-12 21:06:39 -0400

    Added comments explaining the functions and logic of the Home component

--- client/src/components/Home/home.js
+++ client/src/components/Home/home.js
@@ -31,36 +31,75 @@ class Home extends React.Component {
         this.state = {
             displayName: '',
             email: '',
+            artist: '',
+            track: ''
         };
 
-        this.search = this.search.bind(this)
+        this.search = this.search.bind(this);
+        this.artistHandleChange = this.artistHandleChange.bind(this);
+        this.trackHandleChange = this.trackHandleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
 
     };
 
+    // The componentDidMount function will intitiate the Axios GET request once the Home component is rendered
+    // The API path 'users' will return the logged in users Spotify display name and email address
     componentDidMount() {
 
+        // Axios GET request 'users' API path
         axios.get('http://localhost:3000/users')
+
+            // The .then() promise will return the Spotify user data object as the response(res)
+            // ES6 arrow function used to perform the promise
             .then((res) => {
+
+                // Sets the displayName state to the logged in users Spotify display name from response(res)
+                // Sets the email state to the logged in users Spotify email from response(res)
                 this.setState({
                     displayName: res.data.DisplayName,
                     email: res.data.Email
-                })
+                });
             })
+            // If an error occurs during the GET request to the 'users' API it will be caught and logged to the console
             .catch(function (err) {
                 console.log(err)
             });
     };
 
+    artistHandleChange(event) {
+        this.setState({
+            artist: event.target.value
+        });
+    };
+
+    trackHandleChange(event) {
+        this.setState({
+            track: event.targer.value
+        });
+    };
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        var testing = {
+            artist: this.state.artist,
+            track: this.state.track
+        };
+    };
+
     search() {
+         // Axios GET request 'token' API path
         axios.get('http://localhost:3000/token')
             .then(function (res) {
                 console.log(res.data.token)
+
+                // Sets the Spotify access token
                 spotifyApi.setAccessToken(res.data.token);
 
-                // search tracks whose artist's name contains 'Love'
+                // Search artists and their tracks
                 spotifyApi.searchTracks('tupac:temptations')
                     .then(function (data) {
-                        console.log('Search tracks by "Love" in the artist name', data);
+                        console.log(data);
                     }, function (err) {
                         console.error(err);
                     });
@@ -88,8 +127,17 @@ class Home extends React.Component {
                     <h1>Email: {this.state.email}</h1>
                 </div>
 
-                <button onClick={this.search}>Search</button>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Artist:
+                        <input type='text' value={this.state.artist} onChange={this.artistHandleChange} />
+                    </label>
 
+                    <label>
+                        Track:
+                        <input type='text' value={this.state.track} onChange={this.trackHandleChange} />
+                    </label>
+                </form>
 
             </React.Fragment>
         )
