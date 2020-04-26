67e53f4d6823436d061f7e7eab3727dfcf7c1c72
kimani kelly <kimanikelly@yahoo.com>
2020-04-26 13:41:07 -0400

    Deleted CORS functions

--- server.js
+++ server.js
@@ -43,10 +43,6 @@ var express = require('express');
 // Tells node that an express server is being created
 var app = express();
 
-var cors = require('cors');
-
-app.use(cors());
-
 // Sets the port the express server will be running on
 var PORT = process.env.PORT || 3000;
 
