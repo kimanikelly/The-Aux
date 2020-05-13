d046634737fbe8243d743ddbfe141e3225b35583
kimani kelly <kimanikelly@yahoo.com>
2020-05-13 19:48:35 -0400

    Heroku error #26

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -45,7 +45,7 @@ module.exports = function (app) {
         function (req, res) {
 
             // Successful authentication, redirect home.
-            res.redirect('http://localhost:3001/home');
+            res.redirect('/');
         }
     );
 
