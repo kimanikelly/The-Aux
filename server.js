17351ac44edfef4e56e68b033dc181ed3c6ce430
kimani kelly <kimanikelly@yahoo.com>
2020-05-13 22:39:19 -0400

    Heroku error #36

--- server.js
+++ server.js
@@ -140,24 +140,19 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://' + databaseUser + ':' + 
     useUnifiedTopology: true
 })
 
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-    // Although this references the build folder... 
-    // Use the public folder in client/public to publish images/css/any static file
-    // express.static is in charge of sending static files requests to the client.
-    app.use(express.static("client/build"));
-    // client/public is the actual folder to use for static files
-};
+// // Serve up static assets (usually on heroku)
+// if (process.env.NODE_ENV === "production") {
+//     // Although this references the build folder... 
+//     // Use the public folder in client/public to publish images/css/any static file
+//     // express.static is in charge of sending static files requests to the client.
+//     app.use(express.static("client/build"));
+//     // client/public is the actual folder to use for static files
+// };
 
-app.get("*", function (req, res) {
-    if (process.env.NODE_ENV === "production") {
-        res.sendFile(path.join(__dirname, "client/build"));
-    }
-    else {
-        // reminder
-        res.json({ "message": "Go to http://localhost:3001" });
-    }
-});
+if (process.env.NODE_ENV === "production") {
+    res.sendFile(path.join(__dirname, "client/build"));
+    app.use(express.static("client/build"));
+}
 
 // Starts the express server
 app.listen(PORT, function () {
