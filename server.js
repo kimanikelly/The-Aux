1221c5be2ef724d5cb143db7d5323badc7099a8e
kimani kelly <kimanikelly@yahoo.com>
2020-05-14 13:02:35 -0400

    Heroku error #51

--- server.js
+++ server.js
@@ -146,13 +146,13 @@ passport.use(new SpotifyStrategy(
 );
 
 // // // Serve up static assets (usually on heroku)
-// if (process.env.NODE_ENV === "production") {
-//     // Although this references the build folder... 
-//     // Use the public folder in client/public to publish images/css/any static file
-//     // express.static is in charge of sending static files requests to the client.
-//     app.use(express.static("client/build"));
-//     // client/public is the actual folder to use for static files
-// };
+if (process.env.NODE_ENV === "production") {
+    // Although this references the build folder... 
+    // Use the public folder in client/public to publish images/css/any static file
+    // express.static is in charge of sending static files requests to the client.
+    app.use(express.static("client/build"));
+    // client/public is the actual folder to use for static files
+};
 
 app.get("*", (req, res) => {
     if (process.env.NODE_ENV === "production") {
